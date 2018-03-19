69eaba83fc8e3dddd52053458ae29cdba81c2169
Krzysztof Cichopek <krzysztof_cichopek@epam.com>
2018-03-19 11:45:47 +0100

    Correct unit test ConvertTime_GetRightOutput.
    Check for behavior when time argument is invalid in TimeConverter.

--- BerlinClockTests/Tests/TimeConverterTests.cs
+++ BerlinClockTests/Tests/TimeConverterTests.cs
@@ -23,16 +23,24 @@ namespace BerlinClockTests
             _sut = new TimeConverter(_timeValidator.Object, _timeParser.Object);
         }
 
-        [TestCase("13:27:12")]
-        public void ConvertTime_GetRightOutput(string time)
+        [TestCase("13:27:12", ExpectedResult = "Y\r\nRROO\r\nRRRO\r\nYYRYYOOOOOO\r\nYYOO")]
+        public string ConvertTime_GetRightOutput(string time)
         {
             _timeValidator.Setup(x => x.IsValid(time)).Returns(true);
             _timeParser.Setup(x => x.Parse(time)).Returns(new Time(13, 27, 12));
 
-            var asd = _sut.ConvertTime(time);
-            Assert.NotNull(asd);
+            return _sut.ConvertTime(time);
         }
 
-        
+        [TestCase("TestTime")]
+        public void ConvertTime_WhenInputIsInvalid_ThrowArgumenException(string time)
+        {
+            _timeValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(false);
+
+            Assert.Throws<ArgumentException>(() => _sut.ConvertTime(time));
+
+        }
+
+
     }
 }
